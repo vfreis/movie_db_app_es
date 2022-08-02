4a17dc79d0748ea591e418ce05e396231a9898e9
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2022-08-02 18:32:47 -0300

    Update search_category.dart

--- lib/models/search_category.dart
+++ lib/models/search_category.dart
@@ -1,5 +1,5 @@
 class SearchCategory {
   static final String popular = 'Popular';
-  static final String upcoming = 'Upcoming';
-  static final String none = '';
+  static final String upcoming = 'Tendencias';
+  static final String none = 'Por data [BETA]';
 }
